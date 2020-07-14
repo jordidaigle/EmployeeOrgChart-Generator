df62135afcea7660d1be47878bce5698e4cb555c
jordidaigle <jdaigle992@gmail.com>
2020-07-14 16:57:03 -0500

    Working prompt. Working html generation. Adding styling. Missing Employee class. Has all other classes

--- Develop/app.js
+++ Develop/app.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR=path.resolve(__dirname,"output");
 const outputPath=path.join(OUTPUT_DIR,"team.html");
 
 const render=require("./lib/htmlRenderer");
-const teamMembers = [];
+const teamMembers=[];
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -19,11 +19,50 @@ function newEmp() {
     inquirer
         .prompt([
             {
-                type: "input",
-                message: "What role is this for?",
-                name: "role",
-                choices: ['Intern','Manager','Employee']
-            },
+                type: "confirm",
+                message: "Would you like to add an employee?",
+                name: "confirm"
+            }
+        ]).then(response => {
+            if(response.confirm) {
+                createTeam();
+            } else {
+                console.log("Okay!")
+            }
+        });
+
+
+    const createTeam=() => {
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Please select the type of employee you'd like to add.",
+                name: "employeetype",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern",
+                ]
+            }
+        ]).then(response => {
+            switch(response.employeetype) {
+                case "Manager":
+                    addMan();
+                    break;
+                case "Intern":
+                    addInt();
+                    break;
+                case "Engineer":
+                    addEng();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+    }
+
+    const addMan=() => {
+        inquirer.prompt([
             {
                 type: "input",
                 message: "What's the employee's name?",
@@ -44,35 +83,168 @@ function newEmp() {
                 message: "What's the Office Number for the employee?",
                 name: "officeNumber"
             },
-            // {
-            //     type: "input",
-            //     message: "What's the Github username for the employee?",
-            //     name: "github"
-            // },
-            // {
-            //     type: "input",
-            //     message: "What's school does the employee attend?",
-            //     name: "school"
-            // },
-        ])
-
-        .then(response => {
-
-            const manager1 = new Manager(response.name,response.id,response.email,response.officeNumber);
-            teamMembers.push(manager1);
+        ]).then(response => {
+            const manager=new Manager(response.name,response.id,response.email,response.officeNumber);
+            teamMembers.push(manager);
             console.log(teamMembers);
+            addEmp();
 
-            buildTeam();
         })
-            
-            const buildTeam = () => {
-                let renderTeam = render(teamMembers);
-                fs.writeFileSync(outputPath, renderTeam, "utf-8")
-            }
     }
 
+    const addInt=() => {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What's the employee's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What's the ID for the employee?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What's the email for the employee?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What school does this emplyee attend/did they attend?",
+                name: "school"
+            },
+        ]).then(response => {
+            const intern=new Intern(response.name,response.id,response.email,response.school);
+            teamMembers.push(intern);
+            console.log(teamMembers);
+            addEmp();
 
- newEmp();
+        })
+    }
+
+    const addEng=() => {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What's the employee's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What's the ID for the employee?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What's the email for the employee?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What's the Github username for this employee?",
+                name: "github"
+            },
+        ]).then(response => {
+            const engineer=new Engineer(response.name,response.id,response.email,response.github);
+            teamMembers.push(engineer);
+            console.log(teamMembers);
+            addEmp();
+
+        })
+    }
+
+    const addEmp=() => {
+        inquirer.prompt([
+            {
+                type: "confirm",
+                message: "Would you like to add another employee?",
+                name: "confirm",
+            }
+        ]).then(response => {
+            if(response.confirm) {
+                createTeam();
+            } else {
+                console.log("Thank you.")
+                buildTeam();
+            }
+        })
+    }
+
+    const buildTeam=() => {
+        let renderChart=render(teamMembers);
+        fs.writeFileSync(outputPath,renderChart,"utf-8")
+    }
+}
+
+
+newEmp();
+
+
+//     addInt()
+
+//     {
+//         type: "input",
+//             message: "What's the employee's name?",
+//                 name: "name"
+//     },
+//     {
+//         type: "input",
+//             message: "What's the ID for the employee?",
+//                 name: "id"
+//     },
+//     {
+//         type: "input",
+//             message: "What's the email for the employee?",
+//                 name: "email"
+//     },
+//     {
+//         type: "input",
+//             message: "What's school does the employee attend?",
+//                 name: "school"
+//     },
+
+
+// }
+
+
+// addEng(){
+
+//     {
+//         type: "input",
+//             message: "What's the employee's name?",
+//                 name: "name"
+//     },
+//     {
+//         type: "input",
+//             message: "What's the ID for the employee?",
+//                 name: "id"
+//     },
+//     {
+//         type: "input",
+//             message: "What's the email for the employee?",
+//                 name: "email"
+//     },
+//     {
+//         type: "input",
+//             message: "What's the Github username for the employee?",
+//                 name: "github"
+//     },
+
+
+
+    // {
+    //     type: "input",
+    //         message: "What role is this for?",
+    //             name: "role",
+    //                 choices: ['Intern','Manager','Employee']
+    // },
+    //     ])
+
+    //     .then(response => {
+
+
+
 
 
 
